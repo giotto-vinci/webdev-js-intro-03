7690bf500776a2160cb5ed9156b59481bbf71f21
Roderick Fitzgerald <liber6schol@outlook.com>
2025-01-18 23:26:36 -0500

    created .txt file, did experimentation/ commented out, and updated tasks.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,44 @@
 // Prevent us from attempting to use variables
 // that are not declared
 "use strict"
+/*
+console.log(2+2);
+console.log(17-11);
+console.log(2*2);
+console.log(9/3);
+console.log(50%3);
 
+console.log(2 +"2");
+console.log(5+6*7);
+console.log((5+6)*7);
+
+console.log(3 + "5");
+console.log(12/5);
+console.log(12%5);
+
+console.log(2 == 2);
+console.log(2 == "2");
+console.log(2 === "2");
+console.log(2 !== 2);
+console.log(2 !== "2");
+
+let x = 2;
+let y = 7;
+
+console.log(x + y * y);
+console.log(x + y * y % 2);
+console.log((x + y * y) % 2);
+
+console.log(3 || 3);
+console.log(3 || "3");
+console.log(2 || 3);
+console.log(3 || 2);
+console.log(2 && 2);
+console.log(2 && "2");
+console.log(2 && 3);
+console.log(3 && 2);
+
+console.log(1 && 2);
+console.log(1 && 1);
+
+*/
